80d70a158cf61c59afc19e69cd2f580dff50f3ca
wen <bongiovi015@gmail.com>
2018-11-22 10:25:07 +0000

    Clear assets

--- build/src/js/asset-list.js
+++ build/src/js/asset-list.js
@@ -1,7 +1,5 @@
 
 const assetsToLoad = [
-	{"id":"sand_normals","url":"assets/img/sand_normals.png","type":"png"},
-	{"id":"floor","url":"assets/obj/floor.obj","type":"text"},
 	{"id":"pillar","url":"assets/obj/pillar.obj","type":"text"}
 ];
 
